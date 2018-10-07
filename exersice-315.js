fe28f3191cec820012f785ab7f334b5d9ad01a34
Bagus181013 <41609643+Bagus181013@users.noreply.github.com>
2018-10-08 01:36:09 +0700

    Update exersice-315.js

--- exersice-315.js
+++ exersice-315.js
@@ -1,27 +1,21 @@
 function groupAnimals(animals){
-    var group_abjad = [];
-    var group_huruf = [];
-    for(var i = 0; i < animals.length; i++){
-        var cek = false;
-        for(var j = 0; j < group_abjad.length; j++){
-            if(group_abjad[j] === animals[i][0]){
-                cek = true;
+    var result=[]
+    for ( var i=0; i< animals.length; i++){
+        var isEmpty = true
+        var temp=''
+        for ( var j=0; j< result.length; j++){
+            if ( animals[i][0]==result[j][0][0]){
+                isEmpty= false
+                temp=j
             }
         }
-        if(cek === false){
-            group_abjad.push(animals[i][0]);
+        if ( isEmpty === true){
+            result.push([animals[i]])
+        }else{
+            result[temp].push(animals[i])
         }
     }
-    for(var i = 0; i < group_abjad.length; i++){
-        group_huruf.push([]);
-        for(var j = 0; j < animals.length; j++){
-            if(group_abjad[i] === animals[j][0]){
-                group_huruf[i].push(animals[j]);
-            }
-        }
-    }
-    group_huruf.sort();
-    return group_huruf;
+    return result.sort()
 }
 
 // TEST CASES
